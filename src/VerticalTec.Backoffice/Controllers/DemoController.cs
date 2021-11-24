62661a9eba7ba6b0b52058d50ab7a811535e3f0e
Jitthapong Arjsalee <jitthapong@vtec-system.com>
2021-11-24 11:01:04 +0700

    add oidc

--- src/VerticalTec.Backoffice/Controllers/DemoController.cs
+++ src/VerticalTec.Backoffice/Controllers/DemoController.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace VerticalTec.Backoffice.Controllers
@@ -33,6 +37,23 @@ namespace VerticalTec.Backoffice.Controllers
         public IActionResult Chart()
         {
             return View();
+        }
+
+        public async Task<IActionResult> CallApi()
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var content = await client.GetStringAsync("https://localhost:5001/api/identity");
+
+            ViewBag.Json = JArray.Parse(content).ToString();
+            return View("jwt");
+        }
+
+        public IActionResult Logout()
+        {
+            return SignOut("Cookies", "oidc");
         }
     }
 }
--- src/VerticalTec.Backoffice/Startup.cs
+++ src/VerticalTec.Backoffice/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.IdentityModel.Tokens.Jwt;
 using VerticalTec.Backoffice.Models;
 using VerticalTec.Backoffice.Services;
 
@@ -30,6 +31,30 @@ namespace VerticalTec.Backoffice
                         (type, factory) => factory.Create(typeof(SharedResource));
                 });
 
+            JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "Cookies";
+                options.DefaultChallengeScheme = "oidc";
+            })
+            .AddCookie("Cookies")
+            .AddOpenIdConnect("oidc", options =>
+            {
+                options.Authority = "https://localhost:6001";
+
+                options.ClientId = "vtec-backoffice";
+                options.ClientSecret = "3df55b7b9570492c";
+                options.ResponseType = "code";
+
+                options.Scope.Add("profile");
+                options.GetClaimsFromUserInfoEndpoint = true;
+
+                options.SaveTokens = true;
+
+                options.Scope.Add("vtec-api");
+                options.Scope.Add("offline_access");
+            });
+
             services.AddLocalization(options => options.ResourcesPath = "Resources");
             services.ConfigSupportedLanguage();
             services.AddRequestLocalization(options =>
@@ -54,13 +79,15 @@ namespace VerticalTec.Backoffice
 
             app.UseRequestLocalization();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}")
+                    .RequireAuthorization();
             });
         }
     }
